74d0e09f6f3484aa21998dfaba419c8474984975
paulocrazy520 <paulocrazy520@gmail.com>
2023-07-05 03:58:50 +0900

    change to mainnet

--- injective-punk-frontend-react/src/config.js
+++ injective-punk-frontend-react/src/config.js
@@ -1,9 +1,19 @@
-// # Chain config
-export const NEXT_PUBLIC_COINFLIP_CONTRACT=""
-export const NEXT_PUBLIC_CHAIN_ID="injective-888"
-export const NEXT_PUBLIC_CHAIN_NAME="Injective Testnet"
-export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX="inj"
-export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT="https://k8s.testnet.tm.injective.network:443"
-export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT="https://k8s.testnet.lcd.injective.network"
-export const NEXT_PUBLIC_STAKING_DENOM="inj"
+// // # Chain config testnet
+// export const NEXT_PUBLIC_CHAIN_ID = "injective-888";
+// export const NEXT_PUBLIC_CHAIN_NAME = "Injective Testnet";
+// export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX = "inj";
+// export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT =
+//   "https://k8s.testnet.tm.injective.network:443";
+// export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT =
+//   "https://k8s.testnet.lcd.injective.network";
+// export const NEXT_PUBLIC_STAKING_DENOM = "inj";
 
+// # Chain config mainnet
+export const NEXT_PUBLIC_CHAIN_ID = "injective-888";
+export const NEXT_PUBLIC_CHAIN_NAME = "Injective Mainnet";
+export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX = "inj";
+export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT =
+  "https://k8s.global.mainnet.tm.injective.network:443";
+export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT =
+  "https://k8s.global.mainnet.lcd.injective.network:443";
+export const NEXT_PUBLIC_STAKING_DENOM = "inj";
--- injective-punk-frontend-react/src/hooks/cosmwasm.js
+++ injective-punk-frontend-react/src/hooks/cosmwasm.js
@@ -1,50 +1,51 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { toast } from "react-toastify";
-import { connectKeplr } from '../services/keplr'
-import { SigningCosmWasmClient, CosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import { connectKeplr } from "../services/keplr";
+import {
+  SigningCosmWasmClient,
+  CosmWasmClient,
+} from "@cosmjs/cosmwasm-stargate";
 
 import {
   convertMicroDenomToDenom,
   convertDenomToMicroDenom,
-  convertFromMicroDenom
-} from '../util/conversion'
+  convertFromMicroDenom,
+} from "../util/conversion";
 
 import {
   NEXT_PUBLIC_CHAIN_RPC_ENDPOINT,
   NEXT_PUBLIC_CHAIN_REST_ENDPOINT,
   NEXT_PUBLIC_CHAIN_ID,
   NEXT_PUBLIC_STAKING_DENOM,
-  NEXT_PUBLIC_COINFLIP_CONTRACT
-} from '../config'
-import { NotificationManager } from 'react-notifications'
+} from "../config";
+import { NotificationManager } from "react-notifications";
 // import { create } from 'ipfs-http-client'
-import { coin } from '@cosmjs/launchpad'
+import { coin } from "@cosmjs/launchpad";
 
 // import { useNotification } from '../components/Notification'
 
-export const PUBLIC_CHAIN_RPC_ENDPOINT = NEXT_PUBLIC_CHAIN_RPC_ENDPOINT || ''
-export const PUBLIC_CHAIN_REST_ENDPOINT = NEXT_PUBLIC_CHAIN_REST_ENDPOINT || ''
-export const PUBLIC_CHAIN_ID = NEXT_PUBLIC_CHAIN_ID || ''
-export const PUBLIC_STAKING_DENOM = NEXT_PUBLIC_STAKING_DENOM || 'ustars'
-export const PUBLIC_COINFLIP_CONTRACT = NEXT_PUBLIC_COINFLIP_CONTRACT || ''
+export const PUBLIC_CHAIN_RPC_ENDPOINT = NEXT_PUBLIC_CHAIN_RPC_ENDPOINT || "";
+export const PUBLIC_CHAIN_REST_ENDPOINT = NEXT_PUBLIC_CHAIN_REST_ENDPOINT || "";
+export const PUBLIC_CHAIN_ID = NEXT_PUBLIC_CHAIN_ID || "";
+export const PUBLIC_STAKING_DENOM = NEXT_PUBLIC_STAKING_DENOM || "ustars";
 
 export const defaultFee = {
   amount: [],
   gas: "400000",
-}
+};
 
 export const useSigningCosmWasmClient = () => {
-  const [client, setClient] = useState(null)
-  const [signingClient, setSigningClient] = useState(null)
+  const [client, setClient] = useState(null);
+  const [signingClient, setSigningClient] = useState(null);
 
-  const [walletAddress, setWalletAddress] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [walletAddress, setWalletAddress] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  const [nativeBalanceStr, setNativeBalanceStr] = useState('')
-  const [nativeBalance, setNativeBalance] = useState(0)
-  
+  const [nativeBalanceStr, setNativeBalanceStr] = useState("");
+  const [nativeBalance, setNativeBalance] = useState(0);
+
   // const { success: successNotification, error: errorNotification } = useNotification()
 
   ////////////////////////////////////////////////////////////////////////
@@ -55,33 +56,27 @@ export const useSigningCosmWasmClient = () => {
 
   const showNotification = false;
   const notify = (flag, str) => {
-    if (!showNotification)
-      return;
+    if (!showNotification) return;
 
-    if (flag)
-      NotificationManager.success(str)
-    else
-      NotificationManager.error(str)
-  }
+    if (flag) NotificationManager.success(str);
+    else NotificationManager.error(str);
+  };
   const connectWallet = async (inBackground) => {
-    if (!inBackground)
-      setLoading(true)
+    if (!inBackground) setLoading(true);
 
     try {
-      await connectKeplr()
+      await connectKeplr();
 
       // enable website to access kepler
-      await (window).keplr.enable(PUBLIC_CHAIN_ID)
+      await window.keplr.enable(PUBLIC_CHAIN_ID);
 
       // get offline signer for signing txs
-      const offlineSigner = await (window).getOfflineSignerOnlyAmino(
+      const offlineSigner = await window.getOfflineSignerOnlyAmino(
         PUBLIC_CHAIN_ID
-      )
+      );
 
       // make client
-      setClient(
-        await CosmWasmClient.connect(PUBLIC_CHAIN_RPC_ENDPOINT)
-      )
+      setClient(await CosmWasmClient.connect(PUBLIC_CHAIN_RPC_ENDPOINT));
 
       // make client
       setSigningClient(
@@ -89,38 +84,37 @@ export const useSigningCosmWasmClient = () => {
           PUBLIC_CHAIN_RPC_ENDPOINT,
           offlineSigner
         )
-      )
+      );
 
       // get user address
-      const [{ address }] = await offlineSigner.getAccounts()
-      setWalletAddress(address)
+      const [{ address }] = await offlineSigner.getAccounts();
+      setWalletAddress(address);
 
-      localStorage.setItem("address", address)
+      localStorage.setItem("address", address);
 
       if (!inBackground) {
-        setLoading(false)
-        notify(true, "Connected Successfully")
+        setLoading(false);
+        notify(true, "Connected Successfully");
       }
     } catch (error) {
-      notify(false, `Connect error : ${error}`)
+      notify(false, `Connect error : ${error}`);
       if (!inBackground) {
-        setLoading(false)
+        setLoading(false);
       }
     }
-  }
+  };
 
   const disconnect = () => {
     if (signingClient) {
-      localStorage.removeItem("address")
-      signingClient.disconnect()
-
+      localStorage.removeItem("address");
+      signingClient.disconnect();
     }
-    setIsAdmin(false)
-    setWalletAddress('')
-    setSigningClient(null)
-    setLoading(false)
-    notify(true, `Disconnected successfully`)
-  }
+    setIsAdmin(false);
+    setWalletAddress("");
+    setSigningClient(null);
+    setLoading(false);
+    notify(true, `Disconnected successfully`);
+  };
 
   ////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////
@@ -129,18 +123,25 @@ export const useSigningCosmWasmClient = () => {
   ////////////////////////////////////////////////////////////////////////
 
   const getBalances = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const objectNative = await signingClient.getBalance(walletAddress, PUBLIC_STAKING_DENOM)
-      setNativeBalanceStr(`${convertMicroDenomToDenom(objectNative.amount)} ${convertFromMicroDenom(objectNative.denom)}`)
-      setNativeBalance(convertMicroDenomToDenom(objectNative.amount))
-      setLoading(false)
-      notify(true, `Successfully got balances`)
+      const objectNative = await signingClient.getBalance(
+        walletAddress,
+        PUBLIC_STAKING_DENOM
+      );
+      setNativeBalanceStr(
+        `${convertMicroDenomToDenom(
+          objectNative.amount
+        )} ${convertFromMicroDenom(objectNative.denom)}`
+      );
+      setNativeBalance(convertMicroDenomToDenom(objectNative.amount));
+      setLoading(false);
+      notify(true, `Successfully got balances`);
     } catch (error) {
-      setLoading(false)
-      notify(false, `GetBalances error : ${error}`)
+      setLoading(false);
+      notify(false, `GetBalances error : ${error}`);
     }
-  }
+  };
 
   return {
     walletAddress,
@@ -154,6 +155,6 @@ export const useSigningCosmWasmClient = () => {
 
     getBalances,
     nativeBalanceStr,
-    nativeBalance
-  }
-}
+    nativeBalance,
+  };
+};
