de93f77d0fa51d0dd8b3229aa69d57d3aa81b635
paulocrazy520 <paulocrazy520@gmail.com>
2023-07-08 16:36:11 -0400

    update load all nft from contract

--- injective-punk-frontend-react/src/config.js
+++ injective-punk-frontend-react/src/config.js
@@ -1,19 +1,20 @@
 // // # Chain config testnet
-// export const NEXT_PUBLIC_CHAIN_ID = "injective-888";
-// export const NEXT_PUBLIC_CHAIN_NAME = "Injective Testnet";
-// export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX = "inj";
-// export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT =
-//   "https://k8s.testnet.tm.injective.network:443";
-// export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT =
-//   "https://k8s.testnet.lcd.injective.network";
-// export const NEXT_PUBLIC_STAKING_DENOM = "inj";
-
-// # Chain config mainnet
-export const NEXT_PUBLIC_CHAIN_ID = "injective-1";
-export const NEXT_PUBLIC_CHAIN_NAME = "Injective Mainnet";
+export const NEXT_PUBLIC_CHAIN_ID = "injective-888";
+export const NEXT_PUBLIC_CHAIN_NAME = "Injective Testnet";
 export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX = "inj";
 export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT =
-  "https://injectivepunk.com/injective";
+  "https://k8s.testnet.tm.injective.network:443";
 export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT =
-  "https://k8s.global.mainnet.lcd.injective.network:443";
+  "https://k8s.testnet.lcd.injective.network";
 export const NEXT_PUBLIC_STAKING_DENOM = "inj";
+
+export const ContractAddress = process.env.REACT_APP_CONTRACT_ADDRESS;
+// # Chain config mainnet
+// export const NEXT_PUBLIC_CHAIN_ID = "injective-1";
+// export const NEXT_PUBLIC_CHAIN_NAME = "Injective Mainnet";
+// export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX = "inj";
+// export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT =
+//   "https://injectivepunk.com/injective";
+// export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT =
+//   "https://k8s.global.mainnet.lcd.injective.network:443";
+// export const NEXT_PUBLIC_STAKING_DENOM = "inj";
--- injective-punk-frontend-react/src/pages/nfts.js
+++ injective-punk-frontend-react/src/pages/nfts.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Container, Typography } from "@mui/material";
+import { useSigningClient } from "../contexts/cosmwasm";
+import { ContractAddress } from "../config";
+
 
 export default function Nfts() {
+
+  const {
+    walletAddress,
+    connectWallet,
+    signingClient,
+    disconnect,
+    getBalances,
+  } = useSigningClient();
+
+  useEffect(() => {
+    console.log("***********MyNftView -> connectWallet:", walletAddress, ContractAddress);
+
+    if (!walletAddress || !ContractAddress) {
+      console.log("****************No Wallet address or Contract address!!!");
+      return;
+    }
+    if (walletAddress) {
+      loadAllNfts();
+    }
+  }, [walletAddress])
+
+  const loadAllNfts = async () => {
+    const queryHandler = signingClient.queryClient.wasm.queryContractSmart;
+    let entrypointMain = {
+      get_config: {},
+    };
+
+    const responseMain = await queryHandler(ContractAddress, entrypointMain);
+    console.log("*********loading query config:", responseMain);
+
+    const cw721baseAddress = responseMain.cw721_address;
+
+    let r = await queryHandler(cw721baseAddress, {num_tokens:{}});
+    console.log("*********loading num tokens info:", r);  
+
+    let entrypointBase = {
+      all_tokens:{startAfter:"", limit:10000},
+  };
+
+    const responseBase = await queryHandler(cw721baseAddress, entrypointBase);
+    console.log("*********loading all tokens:", responseBase);
+
+    responseBase.tokens.forEach(async element => {
+      let r = await queryHandler(cw721baseAddress, {nft_info:{token_id:element}});
+      console.log("*********loading nft info:", r);  
+    });
+    
+
+
+  }
+
   return (
     <>
       <Container
